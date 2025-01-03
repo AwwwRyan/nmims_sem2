a4ce1d90e3cb56deb11c46c4c9dd02b9c22f582c
Mpstme Student <mpstme.student@SVKMGRP.COM>
2025-01-03 15:16:35 +0530

    react setup working

---
+++ AWT/lab2/my-app/src/hellocomponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hellocomponent() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>This is a new React component.</p>
+        </div>
+    );
+}
+
+export default Hellocomponent;
